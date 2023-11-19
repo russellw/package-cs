2dc1044f3029e293af533fd2861b2b3533ac3a4a
Russell Wallace <russell.wallace@gmail.com>
2023-11-19 13:22:50 +0000

    find csproj

--- Program.cs
+++ Program.cs
@@ -1,5 +1,54 @@
 internal class Program {
 	static void Main(string[] args) {
-		Console.WriteLine("Hello, World!");
+		var options = true;
+		string? csproj = null;
+		foreach (var arg in args) {
+			var s = arg;
+			if (options) {
+				if (s == "--") {
+					options = false;
+					continue;
+				}
+				if (s.StartsWith('-')) {
+					while (s.StartsWith('-'))
+						s = s[1..];
+					switch (s) {
+					case "?":
+					case "h":
+					case "help":
+						Help();
+						return;
+					case "V":
+					case "v":
+					case "version":
+						Console.WriteLine("package-cs " + typeof(Program).Assembly.GetName()?.Version?.ToString(2));
+						return;
+					default:
+						Console.WriteLine(arg + ": unknown option");
+						Environment.Exit(1);
+						break;
+					}
+				}
+			}
+			csproj = s;
+		}
+		if (csproj == null) {
+			foreach (var entry in new DirectoryInfo(".").EnumerateFileSystemInfos())
+				if (entry is FileInfo && entry.Extension == ".csproj") {
+					csproj = entry.FullName;
+					break;
+				}
+			if (csproj == null) {
+				Console.WriteLine("csproj not specified, and not found in current directory");
+				Environment.Exit(1);
+			}
+		}
+	}
+
+	static void Help() {
+		Console.WriteLine("Usage: package-cs [options] [file.csproj]");
+		Console.WriteLine();
+		Console.WriteLine("-h  Show help");
+		Console.WriteLine("-V  Show version");
 	}
 }
