5c63819c641fef1e6a85fdc57c4eec9b6f615593
Russell Wallace <russell.wallace@gmail.com>
2023-11-19 15:24:52 +0000

    batch file

--- Program.cs
+++ Program.cs
@@ -5,6 +5,7 @@ using System.Text.RegularExpressions;
 internal class Program {
 	static void Main(string[] args) {
 		var options = true;
+		var batchFile = false;
 		string? csproj = null;
 		foreach (var arg in args) {
 			var s = arg;
@@ -17,6 +18,9 @@ internal class Program {
 					while (s.StartsWith('-'))
 						s = s[1..];
 					switch (s) {
+					case "b":
+						batchFile = true;
+						break;
 					case "?":
 					case "h":
 					case "help":
@@ -33,6 +37,7 @@ internal class Program {
 						break;
 					}
 				}
+				continue;
 			}
 			if (csproj != null) {
 				Console.WriteLine(s + ": csproj already specified");
@@ -61,24 +66,43 @@ internal class Program {
 		if (process.ExitCode != 0)
 			Environment.Exit(process.ExitCode);
 
+		// Parameters from csproj
 		var targetFramework = "net7.0";
 		var targetFrameworkRegex = new Regex("<TargetFramework>(.*)</TargetFramework>",
 											 RegexOptions.Compiled | RegexOptions.CultureInvariant | RegexOptions.NonBacktracking);
-
-		// Check csproj for parameters
 		var version = "1.0";
+		var versionRegex = new Regex(@"<AssemblyVersion>(\d+\.\d+)\.\d+\.\d+\</AssemblyVersion>",
+									 RegexOptions.Compiled | RegexOptions.CultureInvariant | RegexOptions.NonBacktracking);
 		foreach (var s in File.ReadLines(csproj)) {
 			var match = targetFrameworkRegex.Match(s);
 			if (match.Success)
 				targetFramework = match.Groups[1].Value;
+
+			match = versionRegex.Match(s);
+			if (match.Success)
+				version = match.Groups[1].Value;
+		}
+		var projectName = Path.GetFileNameWithoutExtension(csproj);
+		var projectVersion = $"{projectName}-{version}";
+		var publishPath = $"bin/Release/{targetFramework}/publish";
+
+		// Batch file
+		if (batchFile) {
+			using var writer = new StreamWriter($"{publishPath}/{projectName}.bat");
+			writer.NewLine = "\n";
+			writer.WriteLine("@echo off");
+			writer.WriteLine("rem This file can provide a convenient command to run " + projectName);
+			writer.WriteLine("rem To use it as such,");
+			writer.WriteLine("rem change it to point to where you put your copy of " + projectName);
+			writer.WriteLine("rem and put it in a directory in your PATH");
+			writer.WriteLine($"C:\\{projectVersion}\\{projectName}.exe %*");
 		}
 
 		// Zip
-		var projectVersion = $"{Path.GetFileNameWithoutExtension(csproj)}-{version}";
 		var zipName = $"bin/{projectVersion}.zip";
 		using var zip = File.Create(zipName);
 		using var archive = new ZipArchive(zip, ZipArchiveMode.Update);
-		foreach (var path in Directory.GetFileSystemEntries($"bin/Release/{targetFramework}/publish")) {
+		foreach (var path in Directory.GetFileSystemEntries(publishPath)) {
 			var entry = archive.CreateEntry($"{projectVersion}/{Path.GetFileName(path)}");
 			using var reader = new StreamReader(path);
 			using var writer = new StreamWriter(entry.Open());
@@ -92,5 +116,6 @@ internal class Program {
 		Console.WriteLine();
 		Console.WriteLine("-h  Show help");
 		Console.WriteLine("-V  Show version");
+		Console.WriteLine("-b  Add batch file");
 	}
 }
